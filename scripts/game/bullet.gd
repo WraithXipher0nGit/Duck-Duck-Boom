309786113fc14a2e73eccf76fd3c998dd3e134f8
timur-feix <vfdcz87d2d@privaterelay.appleid.com>
2026-06-05 16:27:06 +0200

    bullet disappears when it leaves the screen

--- scripts/game/bullet.gd
+++ scripts/game/bullet.gd
@@ -1,8 +1,11 @@
 extends Area2D
 @export var bullet_speed = 1200
 
+@onready var notifier: VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+
 func _ready() -> void:
-	area_entered.connect(_on_area_entered) 
+	area_entered.connect(_on_area_entered)
+	notifier.screen_exited.connect(_on_screen_exited) 
 
 
 func _process(delta: float) -> void:
@@ -16,3 +19,7 @@ func _on_area_entered(area: Area2D) -> void:
 		queue_free()
 	elif area.is_in_group("obstacles"):
 		queue_free()
+
+
+func _on_screen_exited() -> void:
+	queue_free()
