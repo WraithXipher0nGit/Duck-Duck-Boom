2165d71d464149f49f5e184e8c17f3e42ad5e8f7
SteezyG <156554267+Stephen-Griffith@users.noreply.github.com>
2026-06-05 22:34:04 -0500

    Audio Update Part 2
    Fixed game_over.gd that I myself messed up.

--- scripts/game/game_over.gd
+++ scripts/game/game_over.gd
@@ -7,6 +7,15 @@ func _ready() -> void:
 	final_score_label.text = "Score: %d" % GameManager.score
 
 
+func _on_play_again_button_mouse_entered() -> void:
+	$UIButtonHover.play()
+
+
+func _on_back_to_menu_button_mouse_entered() -> void:
+	$UIButtonHover.play()
+
+
+
 func _on_play_again_button_pressed() -> void:
 	$UIButtonConfirm.play()
 	await get_tree().create_timer(0.3).timeout
@@ -21,13 +30,3 @@ func _on_back_to_menu_button_pressed() -> void:
 	await get_tree().create_timer(0.3).timeout
 	GameManager.reset()
 	get_tree().change_scene_to_file("res://scenes/menu/main.tscn")
-
-
-func _on_play_again_button_mouse_entered() -> void:
-	$UIButtonHover.play()
-	
-
-
-func _on_back_to_menu_button_mouse_entered() -> void:
-	$UIButtonHover.play()
-	
